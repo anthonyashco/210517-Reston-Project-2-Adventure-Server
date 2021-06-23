02bac3667b2aadba81cf23ed545586a90f0d99e3
jkgmr13 <jokell13552@gmail.com>
2021-06-23 15:39:00 -0600

    added a user entity

---
+++ src/main/java/dev/adventure/entities/User.java
@@ -0,0 +1,93 @@
+package dev.adventure.entities;
+
+public class User {
+    private int id;
+    private String occupation;
+    private String name;
+    private String username;
+    private String passwordHash;
+    private String passwordSalt;
+    private int planId;
+
+    public User() {
+    }
+
+    public User(int id, String occupation, String name, String username, String passwordHash, String passwordSalt, int planId) {
+        this.id = id;
+        this.occupation = occupation;
+        this.name = name;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.passwordSalt = passwordSalt;
+        this.planId = planId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    public int getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(int planId) {
+        this.planId = planId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", occupation='" + occupation + '\'' +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                ", passwordSalt='" + passwordSalt + '\'' +
+                ", planId=" + planId +
+                '}';
+    }
+}
