84f3b918a47610dc4ca2b5532dc9b1afc0b8bc95
jkgmr13 <jokell13552@gmail.com>
2021-06-25 15:45:35 -0600

    add userService Stuff

--- src/main/java/dev/adventure/services/UserServiceImp.java
+++ src/main/java/dev/adventure/services/UserServiceImp.java
@@ -1,2 +1,49 @@
-package dev.adventure.services;public class UserServiceImp {
+package dev.adventure.services;
+
+import dev.adventure.daos.UserDao;
+import dev.adventure.entities.User;
+import dev.adventure.exceptions.EntityNotFoundException;
+import dev.adventure.utils.Password;
+
+import java.util.List;
+
+public class UserServiceImp implements UserService{
+
+    private UserDao userDao;
+
+    public UserServiceImp(UserDao userDao){
+        this.userDao = userDao;
+    }
+
+    @Override
+    public int login(String username, String password) {
+        User user = this.userDao.readUserByUsername(username);
+
+        if(username==null || password==null) throw new EntityNotFoundException("Invalid username or password");
+        if(user==null) throw new EntityNotFoundException("Invalid username or password");
+        String [] hashAndSalt = Password.hashGriddle(password);
+
+        if(Password.checkPass(password, user.getPasswordHash(), user.getPasswordSalt())){
+            return user.getId();
+        }
+        else{
+            throw new EntityNotFoundException("Invalid username or password");
+        }
+
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return this.userDao.readUserById(id);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return this.userDao.readUserByUsername(username);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return this.userDao.readAllUsers();
+    }
 }
