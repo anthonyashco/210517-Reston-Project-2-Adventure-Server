248d9edb4ccb4fcd98ab2a7f53dfcd107aeb7607
eSuminski <rctolinski@gmail.com>
2021-06-25 18:25:30 -0700

    added planController and HTTP routes to app

--- src/main/java/dev/adventure/app/App.java
+++ src/main/java/dev/adventure/app/App.java
@@ -1,7 +1,33 @@
 package dev.adventure.app;
 
+import dev.adventure.controllers.PlanController;
+import dev.adventure.daos.PlanDao;
+import dev.adventure.daos.PlanDaoImp;
+import dev.adventure.services.PlanService;
+import dev.adventure.services.PlanServiceImp;
+import io.javalin.Javalin;
+
 public class App {
-    public static String hello() {
-        return "Hello World!";
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create(config ->{
+           config.enableCorsForAllOrigins();
+           config.enableDevLogging();
+        });
+
+        PlanDao planDao = new PlanDaoImp();
+        PlanService planService = new PlanServiceImp(planDao);
+        PlanController planController = new PlanController(planService);
+
+        app.get("/plans", planController.getAllPlans);
+
+        app.get("plans/:id", planController.getPlanByID);
+
+        app.start();
+
+    }
+
+    public static String hello(){
+        return "Hello world";
     }
 }
