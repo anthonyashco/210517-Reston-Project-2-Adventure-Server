dbcd6fe29c3ab7783eda8656ad34f57ec5a8e51f
eSuminski <rctolinski@gmail.com>
2021-06-28 16:36:23 -0700

    added Manager entity and ManagerDao

---
+++ src/main/java/dev/adventure/daos/ManagerDao.java
@@ -0,0 +1,15 @@
+package dev.adventure.daos;
+
+import dev.adventure.entities.Manager;
+
+import java.util.List;
+
+public interface ManagerDao {
+
+    Manager createManager(Manager manager);
+
+    Manager selectManagerByID(int managerID);
+
+    List<Manager> getAllManagers();
+
+}
---
+++ src/main/java/dev/adventure/entities/Manager.java
@@ -0,0 +1,92 @@
+package dev.adventure.entities;
+
+import dev.adventure.utils.Password;
+import org.jetbrains.annotations.NotNull;
+
+public class Manager implements Comparable<Manager>{
+
+    private int id;
+    private String name;
+    private String username;
+    private String passwordHash;
+    private String passwordSalt;
+
+    public Manager(){}
+
+    public Manager(int id, String name, String username, String passwordHash, String passwordSalt) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.passwordSalt = passwordSalt;
+    }
+
+    public Manager(int id, String name, String username, String password){
+        String [] hash = Password.hashGriddle(password);
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.passwordHash = hash[0];
+        this.passwordSalt = hash[1];
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(@NotNull Manager o) {
+        if(this.id < o.getId()){
+            return -1;
+        }
+        else if(this.id > o.getId()){
+            return 1;
+        }
+        return 0;
+    }
+}
